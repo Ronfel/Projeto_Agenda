ff74374f60c311c183654ba0562fbc8224ccb18d
Ronfel <ronfelara@gmail.com>
2026-09-15 07:09:38 -0300

    Paginacao

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,9 +7,12 @@ def index(request):
     contacts = Contact.objects\
                 .all()\
                 .filter(show=True)
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
