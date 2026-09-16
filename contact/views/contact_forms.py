6cc711a80fa6b0b0bd79ba87290a069b9df2f7fb
Ronfel <ronfelara@gmail.com>
2026-09-16 10:09:56 -0300

    atualizando urls e criando suas views

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,91 @@
+from typing import Any, Dict
+
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from contact.forms import ContactForm
+from contact.models import Contact
+
+def create(request):
+    form_action = reverse('create')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        context = {
+         'form': form,
+         'site_title': 'New Contact - ',
+         'form_action': form_action
+
+        }
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('update', contact_id=contact.id)
+
+        return render(
+            request,
+            'contact/create.html',
+            context=context)
+    
+
+    context = {
+        'form': ContactForm(),
+        'site_title': 'New Contact - ',
+        'form_action': form_action
+
+    }
+    return render(
+        request,
+        'contact/create.html',
+        context=context)
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    form_action = reverse('update', args=(contact_id,))
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        context = {
+         'form': form,
+         'site_title': 'Edit Contact - ',
+         'form_action': form_action
+
+        }
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('update', contact_id=contact.id)
+
+        return render(
+            request,
+            'contact/create.html',
+            context=context)
+    
+
+    context = {
+        'form': ContactForm(instance=contact),
+        'site_title': 'Edit Contact - ',
+        'form_action': form_action
+
+    }
+    return render(
+        request,
+        'contact/create.html',
+        context=context)
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
+    )
