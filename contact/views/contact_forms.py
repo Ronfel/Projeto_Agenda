1a5840bf3ad807bdf21df710b2b570dd4c64af26
Ronfel <ronfelara@gmail.com>
2026-09-17 08:10:56 -0300

    adicionando imagem no contact create

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -8,7 +8,7 @@ from contact.models import Contact
 def create(request):
     form_action = reverse('create')
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
         context = {
          'form': form,
          'site_title': 'New Contact - ',
@@ -42,7 +42,7 @@ def update(request, contact_id):
     form_action = reverse('update', args=(contact_id,))
     
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
         context = {
          'form': form,
          'site_title': 'Edit Contact - ',
